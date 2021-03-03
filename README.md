9a332efe0974a0c10d40f03ebccfd56a55375eed
Matt Lyon <ml18692@bristol.ac.uk>
2021-03-03 15:56:06 +0000

    added scirpt

--- README.md
+++ README.md
@@ -30,14 +30,5 @@ Rscript extract.R
 Run C++ implementation of B-P
 
 ```sh
-module load build/gcc-5.5.0
-~/projects/jlst_cpp/build/bin/jlst_cpp \
--v data/ukb_bmi.txt \
--s , \
--o data/ukb_bmi.vgwas.txt \
--b /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr22.bgen \
--p body_mass_index.21001.0.0 \
--c sex.31.0.0,age_at_recruitment.21022.0.0,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10 \
--i appieu \
--t 1
+bash run.sh
 ```
---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem 12G
+#SBATCH --time=120:00:00
+#SBATCH --partition=mrcieu
+set -euo pipefail
+
+module load build/gcc-5.5.0
+
+# run vGWAS using B-P
+./build/bin/jlst_cpp \
+-v data/ukb_bmi.txt \
+-s , \
+-o data/ukb_bmi.vgwas.t1.txt \
+-b /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr22.bgen \
+-p body_mass_index.21001.0.0 \
+-c sex.31.0.0,age_at_recruitment.21022.0.0,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10 \
+-i appieu \
+-t 1
