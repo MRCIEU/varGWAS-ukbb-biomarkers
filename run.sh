e330897f0146241a1edf977597de9658ad7c1b29
Matt Lyon <ml18692@bc4login1.bc4.acrc.priv>
2021-03-04 21:35:43 +0000

    added performance settings

--- run.sh
+++ run.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem 48G
-#SBATCH --time=120:00:00
+#SBATCH --cpus-per-task=7
+#SBATCH --mem 32G
+#SBATCH --time=72:00:00
 #SBATCH --partition=mrcieu
 set -euo pipefail
 
 module load build/gcc-5.5.0
 
 # set logging level
-export SPDLOG_LEVEL=debug
+#export SPDLOG_LEVEL=debug
+
+# set chromosome
+chr=22
 
 # run vGWAS using B-P
 /mnt/storage/home/ml18692/projects/jlst_cpp/build/bin/jlst_cpp \
 -v data/ukb_bmi.txt \
 -s , \
--o data/ukb_bmi.vgwas.t1.txt \
--b /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr22.bgen \
+-o data/ukb_bmi.vgwas.t6.txt \
+-b /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr"$chr".bgen \
 -p body_mass_index.21001.0.0 \
 -c sex.31.0.0,age_at_recruitment.21022.0.0,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10 \
 -i appieu \
--t 4
+-t 6
