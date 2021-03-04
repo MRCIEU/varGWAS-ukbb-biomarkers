d63e92f47978d2deb263b6322ae3fb9f5b864a39
Matt Lyon <ml18692@bc4login3.bc4.acrc.priv>
2021-03-04 21:03:44 +0000

    updated sc

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=2
-#SBATCH --mem 12G
+#SBATCH --mem 48G
 #SBATCH --time=120:00:00
 #SBATCH --partition=mrcieu
 set -euo pipefail
 
 module load build/gcc-5.5.0
+
+# set logging level
+export SPDLOG_LEVEL=debug
 
 # run vGWAS using B-P
 /mnt/storage/home/ml18692/projects/jlst_cpp/build/bin/jlst_cpp \
@@ -17,4 +20,4 @@ module load build/gcc-5.5.0
 -p body_mass_index.21001.0.0 \
 -c sex.31.0.0,age_at_recruitment.21022.0.0,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10 \
 -i appieu \
--t 1
+-t 4
