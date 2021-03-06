1026ef7116d82939c8e4af449f306a10d506d216
Matt Lyon <ml18692@bc4login1.bc4.acrc.priv>
2021-03-06 17:42:21 +0000

    fix

--- run_R.sh
+++ run_R.sh
@@ -11,6 +11,6 @@ module load languages/r/3.6.0
 # run vGWAS using B-P
 Rscript bp.R \
 -p data/ukb_bmi.txt \
--t "body_mass_index.21001.0.0" \
+-t body_mass_index.21001.0.0 \
 -s data/snps.txt \
 -o data/ukb_bmi.vgwas.r_subsample.txt
