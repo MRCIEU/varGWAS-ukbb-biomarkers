bef03a73646a8fb98793077a0adb39245b103d99
Matt Lyon <ml18692@bristol.ac.uk>
2021-03-22 14:07:23 +0000

    updated

--- extract.R
+++ extract.R
@@ -1,8 +1,15 @@
 load("data/pheno.RData")
 library('data.table')
 library('dplyr')
+library('optparse')
 source("funs.R")
 set.seed(1234)
+
+option_list = list(
+  make_option(c("-t", "--trait"), type="character", default=NULL, help="Variable name for outcome", metavar="character")
+);
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
 
 # export phenotypes for vGWAS
 
@@ -19,14 +26,14 @@ dat <- merge(dat, pheno, by.x="app15825", by.y="eid")
 dat <- merge(dat, pc, "appieu")
 
 # select fields for GWAS
-dat <- dat[,c("appieu", "sex.31.0.0", "age_at_recruitment.21022.0.0", "body_mass_index.21001.0.0", paste0("PC", seq(1, 10))), with=F]
+dat <- dat[,c("appieu", "sex.31.0.0", "age_at_recruitment.21022.0.0", opt$trait, paste0("PC", seq(1, 10))), with=F]
 
 # drop missing values
 dat <- dat[complete.cases(dat), ]
 
 # SD scale
-dat$body_mass_index.21001.0.0 <- dat$body_mass_index.21001.0.0 / sd(dat$body_mass_index.21001.0.0)
+dat[[opt$trait]] <- dat[[opt$trait]] / sd(dat[[opt$trait]])
 dat$age_at_recruitment.21022.0.0 <- dat$age_at_recruitment.21022.0.0 / sd(dat$age_at_recruitment.21022.0.0)
 
 # write out pheno for vGWAS
-write.table(dat, file="data/ukb_bmi.txt", row.names=F, quote=F, sep=",")
+write.table(dat, file=paste0("data/", opt$trait, ".txt", row.names=F, quote=F, sep=",")
--- run_cpp.sh
+++ run_cpp.sh
@@ -8,16 +8,19 @@ set -euo pipefail
 
 module load build/gcc-5.5.0
 
-# set chr from cmd arg
-chr="$1"
+# set trait from args
+trait="$1"
+
+# set chr from args
+chr="$2"
 
 # run vGWAS using B-P
 /mnt/storage/home/ml18692/projects/jlst_cpp/build/bin/jlst_cpp \
--v data/ukb_bmi.txt \
+-v data/"$trait".txt \
 -s , \
--o data/ukb_bmi.vgwas.chr"$chr".txt \
+-o data/"$trait".vgwas.chr"$chr".txt \
 -b /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr"$chr".bgen \
--p body_mass_index.21001.0.0 \
+-p "$trait" \
 -c sex.31.0.0,age_at_recruitment.21022.0.0,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10 \
 -i appieu \
 -t 6
