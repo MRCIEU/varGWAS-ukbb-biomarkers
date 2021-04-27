d42135477dc1efe814cbf5542052cbe85334f394
Matt Lyon <ml18692@bristol.ac.uk>
2021-04-27 15:34:16 +0100

    updated QC

--- qc.R
+++ qc.R
@@ -14,7 +14,7 @@ opt = parse_args(opt_parser);
 trait_name <- opt$trait
 trait_name <- str_split(trait_name, "\\.", simplify = TRUE)[,1]
 trait_name <- gsub("_", " ", trait_name)
-trait_name <- stringr::str_to_title(trait_name)
+trait_name <- str_to_title(trait_name)
 message(paste0("trait ", opt$trait))
 message(paste0("trait name ", trait_name))
 
@@ -38,7 +38,7 @@ gwas <- gwas %>%
 
 # manhattan
 png(paste0("data/", opt$trait, "_manhattan.png"))
-manhattan(gwas, ylim = c(0, 30), chr="CHR", bp="POS", p="P", snp="RSID", main = paste0("vGWAS p-values: ", trait_name))
+manhattan(gwas, ylim = c(0, 30), chr="CHR", bp="POS", p="P", snp="RSID", main = paste0("vGWAS Manhattan plot: ", trait_name))
 dev.off()
 
 # qq plot
