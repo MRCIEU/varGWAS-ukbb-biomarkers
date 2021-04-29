2420e07b51e14e881ed625c7065675dc358a96ba
Matt Lyon <ml18692@bristol.ac.uk>
2021-04-29 10:19:25 +0100

    added bx plot

--- boxplot.R
+++ boxplot.R
@@ -5,8 +5,9 @@ library('ieugwasr')
 library('quantreg')
 library('broom')
 library("robustbase")
+library("scales")
+library("ggplot2")
 source("funs.R")
-source("model.R")
 set.seed(124)
 
 option_list = list(
@@ -14,7 +15,6 @@ option_list = list(
   make_option(c("-t", "--trait"), type="character", default=NULL, help="Name of trait", metavar="character"),
   make_option(c("-c", "--chr"), type="character", default=NULL, help="Chromosome", metavar="character"),
   make_option(c("-b", "--bp"), type="character", default=NULL, help="Position", metavar="character"),
-  make_option(c("-r", "--rsid"), type="character", default=NULL, help="RSID", metavar="character"),
   make_option(c("-n", "--ne"), type="character", default=NULL, help="Other allele", metavar="character"),
   make_option(c("-e", "--ea"), type="character", default=NULL, help="Effect alelle", metavar="character"),
   make_option(c("-o", "--out"), type="character", default=NULL, help="Output file", metavar="character")
@@ -22,10 +22,27 @@ option_list = list(
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
 
+opt <- data.frame(trait="alanine_aminotransferase.30620", chr="4", bp="88212722", ne="G", ea="A", p="data/alanine_aminotransferase.30620.txt", stringsAsFactors=F)
+
 message(paste0("trait ", opt$trait))
 
 # read in extracted phenotypes
-pheno <- fread(opt$p)
+data <- fread(opt$p)
 
-# GWAS
-result <- model(pheno, opt$trait, as.character(opt$chr), as.numeric(opt$bp), as.character(opt$ne), as.character(opt$ea), as.character(opt$rsid))
+# get SNP
+dosage <- extract_variant_from_bgen(as.character(opt$chr), as.numeric(opt$bp), as.character(opt$ne), as.character(opt$ea))
+
+# prepare data
+data <- merge(data, dosage, "appieu")
+data <- na.omit(data)
+s <- paste0("chr", as.character(opt$chr), "_",  as.numeric(opt$bp), "_", as.character(opt$ne), "_", as.character(opt$ea))
+s <- gsub(" ", "", s, fixed = TRUE)
+data[[s]] <- as.factor(round(data[[s]]))
+
+# produce plot
+p <- ggplot(data, aes_string(x=s, y=opt$trait)) + geom_boxplot() + theme_classic() + scale_y_continuous(trans = log2_trans(), breaks = trans_breaks("log2", function(x) 2^x), labels = trans_format("log2", math_format(2^.x)))
+
+# mean and variance by genotype group
+data %>%
+    group_by(chr4_88212722_G_A) %>%
+    summarise(mean = mean(alanine_aminotransferase.30620), var = var(alanine_aminotransferase.30620))
