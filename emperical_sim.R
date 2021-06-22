7c6e4c22de1ebe37aecf9ae381a3a70eaf46ce15
Matt Lyon <ml18692@bristol.ac.uk>
2021-06-22 10:06:23 +0100

    added emperical sim

--- emperical_sim.R
+++ emperical_sim.R
@@ -2,49 +2,48 @@ library("jlst")
 library("data.table")
 library("pwr")
 library("broom")
+library('optparse')
 library("qqman")
 source("funs.R")
 set.seed(123)
 
-#' Estimate treatment effect size a categorical exposure
-#' @param n_obs Number of samples in analysis
-#' @param size Number of categorical levels - 1
-#' @param treatment1_p The probability of recieving treatment
-#' @param sd The SD out of the normal outcome
-#' @param alpha P value threshold
-#' @param power Power to detect effect
-#' @return Effect size
-get_cat_delta <- function(n_obs, size, treatment1_p, sd, alpha, power){
-    p <- power.t.test(n=n_obs * (size * treatment1_p), delta=NULL, sd=sd, sig.level=alpha, power=power, type = c("two.sample"), alternative = c("two.sided"))
-    return(p$delta)
-}
+option_list = list(
+  make_option(c("-t", "--trait"), type="character", default=NULL, help="Name of trait", metavar="character")
+);
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
 
-n_sim <- 10000
-n_obs <- 10000
+#' Function to simulate genotypes in HWE
+#' @param q Recessive/alternative allele frequency
+#' @param n_obs Number of observations to return
+get_simulated_genotypes <- function(q, n_obs){
+  p <- 1 - q
+  x <- sample(c(0, 1, 2), n_obs, prob=c(p^2, 2 * p * q, q^2), replace=T)
+  return(x)
+}
 
 # read in outcome
-dat <- fread("data/aspartate_aminotransferase.30650.0.0.txt")
+dat <- fread(paste0("data/", opt$trait, ".txt")) %>% 
+    pull(opt$trait)
 
-# calculate main effect size for categorical exposure to detect at specified power
-b <- get_cat_delta(n_obs, 2, 0.05, 1, 0.05, 0.8)
-b <- 0
+n_sim <- 1000
+n_obs <- length(dat)
 
 # simulate SNP effect
-p_var <- rep(NA, n_sim)
-p_int <- rep(NA, n_sim)
-for (i in 1:n_sim){
-    x <- rbinom(n=n_obs, size=2, prob=.05)
-    #u <- rbinom(n=n_obs, size=2, prob=.5)
-    y <- x*b + sample(dat$aspartate_aminotransferase.30650.0.0, n_obs)
-    t <- vartest(y, x, type = 1, x.sq = T)
-    p_var[i] <- t$test$P
-    #p_int[i] <- (tidy(lm(y~x*u)) %>% pull(p.value))[4]
+results <- data.frame()
+for (q in c(0.05, 0.1, 0.15, 0.2, 0.25)){
+    for (i in 1:n_sim){
+        x <- get_simulated_genotypes(q, n_obs)
+        y <- x*0 + dat
+        t <- vartest(y, x, type = 1, x.sq = T)
+        results <- rbind(results, data.frame(q, p=t$test$P))
+    }
 }
 
-# estimate power
-tidy(binom.test(sum(p_var < 0.05), n_sim))
-tidy(binom.test(sum(p_int < 0.05), n_sim))
+# estimates T1E for each value of q
+results <- results %>%
+    group_by(q) %>%
+    summarize(tidy(binom.test(sum(p < 0.05), n_sim)))
 
-# plot qq
-qq(p_var)
-qq(p_int)
+# write out results
+write.table(results, sep="\t", quote=F, row.names=F, file=paste0("data/", opt$trait, ".null.txt"))
--- qc.R
+++ qc.R
@@ -1,7 +1,7 @@
 library('data.table')
 library('dplyr')
-library('qqman')
 library('stringr')
+library('ggpubr')
 library('optparse')
 source("funs.R")
 set.seed(1234)
@@ -12,28 +12,106 @@ option_list = list(
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
 
-trait_name <- opt$trait
-trait_name <- str_split(trait_name, "\\.", simplify = TRUE)[,1]
-trait_name <- gsub("_", " ", trait_name)
-trait_name <- str_to_title(trait_name)
-message(paste0("trait ", opt$trait))
-message(paste0("trait name ", trait_name))
+# Taken from https://danielroelfs.com/blog/how-i-create-manhattan-plots-using-ggplot/
+man_plot <- function(gwas_data, sig=5e-8/30, ylim=30){
+    data_cum <- gwas_data %>% 
+        group_by(chr) %>% 
+        summarise(max_bp = max(pos)) %>% 
+        mutate(bp_add = lag(cumsum(as.numeric(max_bp)), default = 0)) %>% 
+        select(chr, bp_add)
 
-# load vGWAS and QC
+    gwas_data <- gwas_data %>% 
+        inner_join(data_cum, by = "chr") %>% 
+        mutate(bp_cum = pos + bp_add)
+
+    axis_set <- gwas_data %>% 
+        group_by(chr) %>% 
+        summarize(center = mean(bp_cum))
+
+    gwas_data$p <- -log10(gwas_data$p)
+
+    manhplot <- ggplot(gwas_data, aes(x = bp_cum, y = p, color = as.factor(chr), size = p)) +
+        geom_point(alpha = 0.75) +
+        geom_hline(yintercept = -log10(sig), color = "grey40", linetype = "dashed") + 
+        scale_x_continuous(label = axis_set$chr, breaks = axis_set$center) +
+        scale_y_continuous(expand = c(0,0), limits = c(0, ylim)) +
+        scale_color_manual(values = rep(c("#276FBF", "#183059"), unique(length(axis_set$chr)))) +
+        scale_size_continuous(range = c(0.5,3)) +
+        labs(x = NULL, y = expression(paste("-log"[10],"(", plain(P),")"))) + 
+        theme_minimal() +
+        theme( 
+            legend.position = "none",
+            axis.title.x=element_blank(),
+            axis.text.x=element_blank(),
+            axis.ticks.x=element_blank()
+        )
+
+    return(manhplot)
+}
+
+# Taken from https://danielroelfs.com/blog/how-i-make-qq-plots-using-ggplot/
+qq_plot_dat <- function(sumstats.data, pval_col, outcome, ci=0.95){
+    nSNPs <- nrow(sumstats.data)
+    plotdata <- data.frame(
+        observed = -log10(sort(sumstats.data[[pval_col]])),
+        expected = -log10(ppoints(nSNPs)),
+        clower   = -log10(qbeta(p = (1 - ci) / 2, shape1 = seq(nSNPs), shape2 = rev(seq(nSNPs)))),
+        cupper   = -log10(qbeta(p = (1 + ci) / 2, shape1 = seq(nSNPs), shape2 = rev(seq(nSNPs)))),
+        outcome
+    )
+    return(plotdata)
+}
+
+# Taken from https://danielroelfs.com/blog/how-i-make-qq-plots-using-ggplot/
+qq_plot <- function(plotdata, ldsc=NULL){
+    qqplot <- ggplot(plotdata, aes(x = expected, y = observed)) +
+        geom_ribbon(aes(ymax = cupper, ymin = clower), fill = "red", alpha = 0.5) +
+        geom_point() +
+        labs(x = expression(paste("Expected -log"[10],"(", plain(P),")")),
+            y = expression(paste("Observed -log"[10],"(", plain(P),")"))) +
+        theme_minimal() +
+        scale_x_continuous(labels = scales::comma, breaks = scales::pretty_breaks(n = 3)) +
+        scale_y_continuous(labels = scales::comma, breaks = scales::pretty_breaks(n = 2))
+    if(!is.null(ldsc)){
+        qqplot <- qqplot +
+          geom_text(data = ldsc, aes(label = lab, x = -Inf, y = Inf), hjust = 0, vjust = 1, parse=T)
+    }
+    return(qqplot)
+}
+
+# load LDSC results
+ldsc <- fread("data/ldsc.txt")
+names(ldsc) <- c("trait", "intercept", "se")
+ldsc$lci <- ldsc$intercept - (1.96 * ldsc$se)
+ldsc$uci <- ldsc$intercept + (1.96 * ldsc$se)
+ldsc$lab <- paste0("lambda == ", sprintf('%.2f',ldsc$intercept), "  (", sprintf('%.2f',ldsc$lci), "-", sprintf('%.2f',ldsc$uci), ")")
+ldsc <- ldsc[ldsc$trait %in% biomarkers]
+ldsc$outcome <- sapply(ldsc$trait, function(x) biomarkers_abr[x == biomarkers])
+
+trait_name <- get_trait_name(opt$trait)
+abr <- biomarkers_abr[biomarkers == opt$trait]
+
+# load vGWAS
 data <- get_variants(opt$trait)
 
-# manhattan
-png(paste0("data/", opt$trait, "_phi_manhattan.png"))
-manhattan(data, ylim = c(0, 25), chr="chr", bp="pos", p="phi_p", snp="rsid", main = trait_name)
-dev.off()
-png(paste0("data/", opt$trait, "_beta_manhattan.png"))
-manhattan(data, ylim = c(0, 25), chr="chr", bp="pos", p="p", snp="rsid", main = trait_name)
+# calculate params for QQ plot
+qq_var <- qq_plot_dat(data, "phi_p", abr)
+qq_mu <- qq_plot_dat(data, "p", abr)
+man_var <- data %>% select(chr, pos, phi_p) %>% rename(p=phi_p) %>% mutate(trait = abr)
+man_mu <- data %>% select(chr, pos, p) %>% mutate(trait = abr)
+
+png(paste0("data/", opt$trait, "_gwas_qq_var.png"))
+qq_plot(qq_var)
 dev.off()
 
-# qq plot
-png(paste0("data/", opt$trait, "_phi_qq.png"))
-qq(data$phi_p, main = trait_name)
+png(paste0("data/", opt$trait, "_gwas_qq_mu.png"))
+qq_plot(qq_mu, ldsc[ldsc$trait == opt$trait])
 dev.off()
-png(paste0("data/", opt$trait, "_beta_qq.png"))
-qq(data$p, main = trait_name)
+
+png(paste0("data/", opt$trait, "_gwas_man_var.png"))
+man_plot(man_var)
+dev.off()
+
+png(paste0("data/", opt$trait, "_gwas_man_mu.png"))
+man_plot(man_mu)
 dev.off()
