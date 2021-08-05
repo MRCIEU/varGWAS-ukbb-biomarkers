0c61a7b027c111476866930de976afd5f9e60399
Matt Lyon <ml18692@bristol.ac.uk>
2021-08-05 14:15:59 +0100

    updated gxg plot

--- gxg_plot.R
+++ gxg_plot.R
@@ -118,9 +118,9 @@ multiplicative <- merge(multiplicative, fread("data/gxg.txt") %>% mutate(tt=past
 
 # save plot
 pdf("gxg-additive.pdf", height=6, width=8)
-print(get_plot(additive, "Multiplicative (P < 5e-5"))
+print(get_plot(additive, "Multiplicative (P < 5e-5)"))
 dev.off()
 
 pdf("gxg-multiplicative.pdf", height=5, width=8)
-print(get_plot(multiplicative, "Additive (P < 5e-5"))
+print(get_plot(multiplicative, "Additive (P < 5e-5)"))
 dev.off()
