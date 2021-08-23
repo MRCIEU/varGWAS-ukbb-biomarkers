9e996b508531d3f4b0132e4f7d96ca0f98332353
Matt Lyon <ml18692@bristol.ac.uk>
2021-08-23 14:46:48 +0100

    updated gxg-qual

--- gxg-qual.R
+++ gxg-qual.R
@@ -47,20 +47,18 @@ snps$key <- paste0("chr", snps$chr, "_", snps$pos, "_", snps$oa, "_", snps$ea)
 for (i in 1:nrow(snps)){
     dosage <- extract_variant_from_bgen(as.character(snps$chr[i]), as.double(snps$pos[i]), snps$oa[i], snps$ea[i])
     pheno <- merge(pheno, dosage, "appieu")
+    # create dichotomised modifiers
+    pheno[[paste0(snps$key[i], "_bin")]] <- pheno[[snps$key[i]]] %>% round %>% dplyr::recode(`2`=1) %>% as.logical
 }
 
-# test for interaction between each snp
+# test for effect of SNP stratified by modifier
 results <- data.frame()
 for (i in 1:nrow(d)){
-  message("Testing GxG for: ", d$V1[i], " ", d$V2[i])
-
-  # test GxG on additive scale
-  f <- as.formula(paste0(opt$trait, " ~ age_at_recruitment.21022.0.0 + sex.31.0.0 + PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10 + ", paste0(d$V1[i], " * " ,d$V2[i], collapse=" + ")))
+  # test SNP effect by stratified modifier
+  f <- as.formula(paste0(opt$trait, " ~ ", d$V1[i], " + age_at_recruitment.21022.0.0 + sex.31.0.0 + PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10"))
   t <- tidy(lmrob(f, data=pheno))
-
-  # store results
   results <- rbind(results, t[grep(":", t$term),])
 }
 
 # save
-write.table(results, sep="\t", quote=F, row.names=F, file=paste0("data/", opt$trait, ".gxg.txt"))
+write.table(results, sep="\t", quote=F, row.names=F, file=paste0("data/", opt$trait, ".gxg-qual.txt"))
