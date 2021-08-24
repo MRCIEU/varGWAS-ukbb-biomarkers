13ce277fdb9bbfafddbbbb3d7a2e193958d5350d
Matt Lyon <ml18692@bristol.ac.uk>
2021-08-24 16:52:41 +0100

    updated gxg-qual

--- gxg-qual.R
+++ gxg-qual.R
@@ -54,6 +54,9 @@ for (i in 1:nrow(snps)){
 # test for effect of SNP stratified by modifier
 results <- data.frame()
 for (i in 1:nrow(d)){
+  if (d$trait[i] != opt$trait){
+    next
+  }
   # test SNP effect by stratified modifier
   f <- as.formula(paste0(opt$trait, " ~ ", d$V1[i], " + age_at_recruitment.21022.0.0 + sex.31.0.0 + PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10"))
   k <- paste0(d$V2[i], "_bin")
