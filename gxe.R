0a6c27ac80a32ce58e92727e827062d12719dae2
Matt Lyon <ml18692@bristol.ac.uk>
2021-09-21 13:39:22 +0100

    updated gxe

--- gxe.R
+++ gxe.R
@@ -4,7 +4,6 @@ library('data.table')
 library('dplyr')
 library('broom')
 library('ieugwasr')
-library('robustbase')
 source("funs.R")
 set.seed(1234)
 options(ieugwasr_api="http://64.227.44.193:8006/")
@@ -35,8 +34,9 @@ dat <- merge(dat, pc, "appieu")
 for (e in env_exp){
   dat[[e]] <- dat[[e]] / sd(dat[[e]], na.rm=T)
 }
-dat[[opt$trait]] <- dat[[opt$trait]] / sd(dat[[opt$trait]], na.rm=T)
 dat[[paste0(opt$trait, "_log")]] <- log(dat[[opt$trait]])
+dat[[opt$trait]] <- dat[[opt$trait]] / sd(dat[[opt$trait]], na.rm=T)
+dat[[paste0(opt$trait, "_log")]] <- dat[[paste0(opt$trait, "_log")]] / sd(dat[[paste0(opt$trait, "_log")]], na.rm=T)
 
 # read in clumped vQTLs
 snps <- fread(paste0("data/", opt$trait, ".clump.txt"))
@@ -63,16 +63,16 @@ for (i in env_exp){
     message("Testing GxE for: ", i, " ", vqtls[j])
 
     f <- as.formula(paste0(opt$trait, " ~ age_at_recruitment.21022.0.0 + sex.31.0.0 + PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10 + ", paste0(i, " * " ,vqtls[j], collapse=" + ")))
-    t <- tidy(lmrob(f, data=dat))
-
-    # store results
-    results <- rbind(results, t[grep(":", t$term),])
+    mod <- lm(f, data=pheno)
+    t <- coeftest(mod, vcov = vcovHC(mod, type = "HC0")) %>% tidy
+    t <- t %>% dplyr::filter(grepl(":", t$term))
+    results <- rbind(results, t)
 
     f <- as.formula(paste0(opt$trait, "_log ~ age_at_recruitment.21022.0.0 + sex.31.0.0 + PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10 + ", paste0(i, " * " ,vqtls[j], collapse=" + ")))
-    t <- tidy(lmrob(f, data=dat))
-
-    # store results
-    results_log <- rbind(results_log, t[grep(":", t$term),])
+    mod <- lm(f, data=pheno)
+    t <- coeftest(mod, vcov = vcovHC(mod, type = "HC0")) %>% tidy
+    t <- t %>% dplyr::filter(grepl(":", t$term))
+    results_log <- rbind(results_log, t)
   }
 }
 
