5043cd42f2da79e8e534735db56a5525821f175d
Matt Lyon <ml18692@bristol.ac.uk>
2021-11-25 14:17:24 +0000

    updated GWAS

--- run_vargwas.sh
+++ run_vargwas.sh
@@ -6,7 +6,7 @@
 #SBATCH --partition=mrcieu
 set -euo pipefail
 
-module load build/gcc-7.2.0
+module load languages/gcc/9.3.0
 
 # set trait from args
 trait="$1"
@@ -15,14 +15,13 @@ trait="$1"
 chr="$2"
 
 # run vGWAS
-/mnt/storage/home/ml18692/projects/varGWAS-1.1/varGWAS/build/bin/varGWAS \
+/mnt/storage/home/ml18692/projects/varGWAS/bin/varGWAS \
 -v data/"$trait".txt \
 -s , \
 -o data/"$trait".vgwas.chr"$chr".txt \
 -b /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr"$chr".bgen \
 -p "$trait" \
--c sex.31.0.0,age_at_recruitment.21022.0.0,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10 \
+-c sex.31.0.0,age_at_recruitment.21022.0.0,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10,age_at_recruitment.21022.0.0_sq,PC1_sq,PC2_sq,PC3_sq,PC4_sq,PC5_sq,PC6_sq,PC7_sq,PC8_sq,PC9_sq,PC10_sq \
 -i appieu \
 -t 6 \
--r \
 -m 0.05
