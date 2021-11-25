20df2adfd6848fe44f9e7c51c48eb2f339502087
Matt Lyon <ml18692@bristol.ac.uk>
2021-11-25 16:15:30 +0000

    updated gwas

--- run_vargwas.sh
+++ run_vargwas.sh
@@ -18,7 +18,7 @@ chr="$2"
 /mnt/storage/home/ml18692/projects/varGWAS/bin/varGWAS \
 -v data/"$trait".txt \
 -s , \
--o data/"$trait"_noxsq.vgwas.chr"$chr".txt \
+-o data/"$trait".vgwas.chr"$chr".txt \
 -b /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr"$chr".bgen \
 -p "$trait" \
 -c sex.31.0.0,age_at_recruitment.21022.0.0,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10 \
