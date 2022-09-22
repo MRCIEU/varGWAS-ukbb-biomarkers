01e7b0e75d15ffd3c3e3d84b516d063f61cacdb8
Matt Lyon <ml18692@bristol.ac.uk>
2022-09-22 09:46:41 +0100

    updated rlang

--- runR.sh
+++ runR.sh
@@ -6,7 +6,7 @@
 #SBATCH --partition=mrcieu,cpu
 set -euo pipefail
 
-module load languages/r/4.1.2
+module load languages/r/3.6.0
 module load apps/plink/1.90
 PATH=$PATH:/mnt/storage/home/ml18692/scratch/db/locuszoom/locuszoom/bin
 
